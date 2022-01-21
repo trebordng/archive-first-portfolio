ed903075f7b27a1b9b6565ba6da704aff45dc8b3
trebordng <rtran.nvtt@gmail.com>
2022-01-22 03:34:36 +1000

    change

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,37 +6,40 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 function Navigation() {
   return (
     <>
-      <Navbar>
+      <Navbar expand="lg">
         <Container>
           <Navbar.Brand>
             <Link to="/Home">#robertTran</Link>
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to="/Home">Home</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Professional">Professional</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Portfolio">Portfolio</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Education">Education</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Experience">Experience</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Contact">Contact</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/Blog">Blog</Link>
-            </Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/Home">Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Professional">Professional</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Portfolio">Portfolio</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Education">Education</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Experience">Experience</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Contact">Contact</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/Blog">Blog</Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Outlet/>
+      <Outlet />
     </>
   );
 }
