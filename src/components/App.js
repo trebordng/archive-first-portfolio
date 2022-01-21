c9ce9051a6e4af4808d505ff357f09190f53c170
trebordng <rtran.nvtt@gmail.com>
2022-01-21 14:15:51 +1000

    Trigger rebuild

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "./Navigation/Navigation";
 import Home from "./Home/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Professional from "./Professional/Professional";
 
 function App() {
